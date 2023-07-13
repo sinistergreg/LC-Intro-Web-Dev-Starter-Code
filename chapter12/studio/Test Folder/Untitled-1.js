8d5d0cbecef4b5fe124d4f8fcb72431e321e74b7
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-07-13 09:38:47 -0600

    test

---
+++ chapter12/studio/Test Folder/Untitled-1.js
@@ -0,0 +1,17 @@
+
+
+
+function calculateArea(width,height) {
+    area =width*height
+    return area
+}
+
+console.log(calculateArea(15,17))
+
+
+fucntion sumArray
+
+
+// Write a function called calculateArea that takes two arguments, width and height, and returns the area of a rectangle.
+// Create a function called greetUser that takes a name argument and returns a greeting message including the name.
+// Write a function named sumArray that accepts an array of numbers as an argument and returns the sum of all the numbers in the array.
