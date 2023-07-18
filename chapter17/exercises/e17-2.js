b9ee0230934b922b72e4d6e41797a43ce824d5ad
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-07-18 09:44:02 -0600

    Chapter 17 ex 2

--- chapter17/exercises/e17-2.js
+++ chapter17/exercises/e17-2.js
@@ -1,26 +1,54 @@
 // 17.4.2. Test Student Labs
 
 function gradeLabs(labs) {
-    for (let i=0; i < labs.length; i++) {
-      let lab = labs[i];
-      let result = lab.runLab(3);
-      console.log(`${lab.student} code worked: ${result === 27}`);
+  for (let i=0; i < labs.length; i++) {
+    let lab = labs[i];
+    let result = lab.runLab(3);
+    console.log(`${lab.student} code worked: ${result === 27}`);
+  }
+}
+
+let studentLabs = [
+  {
+    student: 'Carly',
+    runLab: function (num) {
+        return Math.pow(num, num);
+    }
+  },
+  {
+    student: 'Erica',
+    runLab: function (num) {
+        return num * num;
     }
   }
-  
-  let studentLabs = [
-    {
-      student: 'Carly',
-      runLab: function (num) {
-          return Math.pow(num, num);
+];
+
+gradeLabs(studentLabs);
+
+
+let studentLabs2 = [
+   {
+      student: 'Blake',
+      myCode: function (num) {
+         return Math.pow(num, num);
       }
-    },
-    {
-      student: 'Erica',
+   },
+   {
+      student: 'Jessica',
       runLab: function (num) {
-          return num * num;
+         return Math.pow(num, num);
       }
-    }
-  ];
-  
-  gradeLabs(studentLabs);
+   },
+   {
+      student: 'Mya',
+      runLab: function (num) {
+         return num * num;
+      }
+   }
+];
+
+try {
+   console.log('runLab function:', studentLabs[index].runLab);
+} catch(err) {
+   console.log("Error Thrown!!! ");
+};
